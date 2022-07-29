33e3f186f227d960a69b6bf57b6ec8d642ee3248
kotaro-yazaki <77482738+kotaro-yazaki@users.noreply.github.com>
2022-07-29 17:30:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const nav = document.querySelector('#js-nav');
 
 ham.addEventListener('click', function () {
 
+  console.log('ok!');
   ham.classList.toggle('active');
   nav.classList.toggle('active');
 
