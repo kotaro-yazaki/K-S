47d58eee941bd52d2946b8353c5a1ed6481820f8
kotaro-yazaki <77482738+kotaro-yazaki@users.noreply.github.com>
2022-07-29 19:20:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const ham = document.querySelector('#js-hamburger');
-const nav = document.querySelector('#js-nav');
-
-ham.addEventListener('click', function () {
-
-  console.log('ok!');
-  ham.classList.toggle('active');
-  nav.classList.toggle('active');
-
-});
-
-/*
 $(function(){
   $('.index-btn').click(function() {
     $('.active').removeClass('active');
@@ -26,4 +14,14 @@ $(function(){
 
 
 }); 
-*/
+
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function () {
+
+  console.log('ok!');
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+
+});
