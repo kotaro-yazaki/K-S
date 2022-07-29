e3e2e0449105ec50758ba9ee5e7893fc59052e9b
kotaro-yazaki <77482738+kotaro-yazaki@users.noreply.github.com>
2022-07-29 17:19:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,16 @@ $(function(){
       "scrollTop":0
     },500);
   });
-  
-  const ham = document.querySelector('#js-hamburger');
-  const nav = document.querySelector('#js-nav');
-
-  ham.addEventListener('click', function () {
-
-    ham.classList.toggle('active');
-    nav.classList.toggle('active');
-
-  });
-
 
 
 }); 
 
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function () {
+
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+
+});
